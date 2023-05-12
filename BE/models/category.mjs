55024a65e28d82cb439817f259bd4834f5908fe7
Trần Nam Tuấn <tuantran1632001@gmail.com>
2023-05-12 14:05:50 +0700

    Add db models

---
+++ BE/models/category.mjs
@@ -0,0 +1,14 @@
+import mongoose from 'mongoose';
+const { Schema } = mongoose;
+
+const CategorySchema = new Schema({
+    name: { type: String, required: true, trim: true }
+});
+
+const CategoryTreeSchema = new Schema({
+    parentId: { type: Schema.Types.ObjectId, ref: 'Category' },
+    childId: { type: Schema.Types.ObjectId, ref: 'Category' }
+})
+
+exports.Category = mongoose.model("Category", CategorySchema);
+exports.CategoryTree = mongoose.model("CategoryTree", CategoryTreeSchema);
---
+++ BE/models/user.mjs
@@ -0,0 +1,28 @@
+import mongoose from 'mongoose';
+const { Schema } = mongoose;
+
+const phoneRegEx = /^(\()?\d{3}(\))?(-|\s)?\d{3}(-|\s)\d{4}$/
+const emailRegEx = /^\w+([\.-]?\w+)*@\w+([\.-]?\w+)*(\.\w+)+$/
+
+const UserSchema = new Schema({
+    username: { type: String, required: true, trim: true }, // TODO: write validator
+    password: { type: String, required: true }, // TODO: write validator and hash
+    type: { type: String, required: true }, // Should be an enum
+    phone: {
+        type: String,
+        required: true,
+        validate: phone => {
+            return phoneRegEx.test(phone);
+        },
+        match: phoneRegEx // https://stackoverflow.com/questions/66383516/add-mongoose-validation-for-phone-numbers
+    },
+    email: {
+        type: String,
+        required: true,
+        validate: email => {
+            return emailRegEx.test(email);
+        },
+        match: emailRegEx // https://stackoverflow.com/questions/18022365/mongoose-validate-email-syntax
+    },
+    address: { type: String, required: true, trim: true }
+});
