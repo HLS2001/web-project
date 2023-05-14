65bed1605cfe86f5d9feae968f9f6c24fb0b2c59
Trần Nam Tuấn <tuantran1632001@gmail.com>
2023-05-14 21:01:07 +0700

    Finished all account api method

--- BE/routes/account.mjs
+++ BE/routes/account.mjs
@@ -2,10 +2,10 @@ import express from 'express';
 import User from '../models/user.mjs';
 const router = express.Router();
 
-router.get('/all', async function (req, res) {
+router.get('/find', async function (req, res) {
     res.setHeader('Connection', 'close');
 
-    if (!req.session.loggedIn) {
+    if (!req.session.userId) {
         res.status(401).end();
         return;
     }
@@ -16,11 +16,28 @@ router.get('/all', async function (req, res) {
         return;
     }
 
-    res.send(await User.find().exec());
+    if (Object.keys(req.params).length === 0) {
+        res.send(await User.find().exec());
+    } else {
+        let filter = {};
+        for (let key in req.params) {
+            if (key === 'id') {
+                filter['_id'] = req.params.id;
+            } else {
+                filter[key] = req.params[key];
+            }
+        }
+        res.send(await User.find(filter).exec());
+    }
 });
 
 router.post('/login', async function (req, res) {
     res.setHeader('Connection', 'close');
+
+    if (req.session.userId) {
+        res.status(400).send('User is already logged in');
+        return;
+    }
 
     let username = req.body.username;
     let password = req.body.password;
@@ -31,7 +48,6 @@ router.post('/login', async function (req, res) {
             password: password,
         }).exec();
         if (query) {
-            req.session.loggedIn = true;
             req.session.userId = query._id;
 
             res.status(200).send();
@@ -46,9 +62,10 @@ router.post('/login', async function (req, res) {
 router.post('/logout', async function (req, res) {
     res.setHeader('Connection', 'close');
 
-    if (req.session.loggedin) {
+    if (req.session.userId) {
         try {
             req.session.destroy();
+            res.status(200).end();
         } catch (error) {
             res.status(400).send(error);
         }
@@ -60,7 +77,7 @@ router.post('/logout', async function (req, res) {
 router.post('/register', async function (req, res) {
     res.setHeader('Connection', 'close');
 
-    if (req.session.loggedIn) {
+    if (req.session.userId) {
         res.status(400).send('User is logging in');
         return;
     }
@@ -76,11 +93,125 @@ router.post('/register', async function (req, res) {
 
     try {
         await newUser.save();
+        res.status(200).end();
     } catch (error) {
-        res.status(400).end();
+        res.status(400).send(error);
         return;
     }
-    res.status(200);
+});
+
+router.post('/create', async function (req, res) {
+    res.setHeader('Connection', 'close');
+
+    const query = await User.findById(req.session.userId).exec();
+    if (query.type !== 'Admin') {
+        res.status(403);
+        return;
+    }
+
+    const newUser = new User({
+        username: req.body.username,
+        password: req.body.password,
+        type: req.body.type,
+        phone: req.body.phone,
+        email: req.body.email,
+        address: req.body.address,
+    });
+
+    try {
+        await newUser.save();
+        res.status(200).end();
+    } catch (error) {
+        res.status(400).send(error);
+        return;
+    }
+});
+
+router.post('/edit', async function (req, res) {
+    res.setHeader('Connection', 'close');
+
+    if (!req.session.userId) {
+        res.status(401).end();
+        return;
+    }
+
+    if (req.params.id) {
+        const query = await User.findByIdAndUpdate(
+            req.params.id,
+            { $set: req.body },
+            { new: true }
+        ).exec();
+        if (query) {
+            res.status(200).send(query);
+        } else {
+            res.status(404).send('User not found');
+        }
+    } else {
+        res.status(400).send('Missing id');
+    }
+});
+
+router.post('/delete', async function (req, res) {
+    res.setHeader('Connection', 'close');
+
+    if (!req.session.userId) {
+        res.status(401).end();
+        return;
+    }
+
+    if (!req.params.id) {
+        res.status(400).send('Missing id');
+        return;
+    }
+
+    let id;
+    if (req.params.id === req.session.userId) {
+        // User delete their own account
+        id = req.params.id;
+    } else {
+        // Only admin can delete other user account
+        const query = await User.findById(req.session.userId).exec();
+        if (query.type !== 'Admin') {
+            res.status(403).send('Only admin can delete other user');
+            return;
+        }
+    }
+
+    const query = await User.findByIdAndDelete(req.params.id).exec();
+    if (query) {
+        req.status(200).end();
+    } else {
+        req.status(400).send('User doesn\t exist');
+    }
+});
+
+router.get('/info', async function (req, res) {
+    res.setHeader('Connection', 'close');
+
+    if (!req.session.userId) {
+        res.status(401).end();
+        return;
+    }
+
+    if (!req.params.id) {
+        const query = User.findById(req.session.userId).exec();
+        res.status(200).send(query);
+        return;
+    }
+
+    // Only admin can view other user account
+    const queryAdmin = await User.findById(req.session.userId).exec();
+    if (queryAdmin.type !== 'Admin') {
+        res.status(403).send('Only admin can view other user account');
+        return;
+    }
+
+    const queryInfo = await User.findById(req.params.id).exec();
+    if (queryInfo) {
+        res.status(200).send(queryInfo);
+    } else {
+        res.status(404).send('User not found');
+    }
 });
 
 export default router;
