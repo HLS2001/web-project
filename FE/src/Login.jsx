373324e8170af926faafb1be8a9bbaa577f09d19
huynhleson <80943786+huynhleson@users.noreply.github.com>
2023-05-22 16:45:53 +0700

    Add files via upload

--- FE/src/Login.jsx
+++ FE/src/Login.jsx
@@ -1,4 +1,25 @@
+import { useCallback, useRef } from "react";
+import axios from 'axios'
+
 function Login() {
+  const uniRef = useRef()
+  const pdRef = useRef()
+
+  const handleLogin = useCallback(async () => {
+    const uni =  uniRef.current
+    const pd = pdRef.current
+    if(!uni || !pd ) return
+    try {
+      const res = await axios.post(`${import.meta.env.VITE_BE_URI}account/login`,{
+        username: uni.value,
+        password: pd.value
+      })
+      console.log(res); 
+    } catch (error) {
+      console.error(error);
+    }
+  },[])
+
   return (
     <div className="Login">
       <div className="box-form">
@@ -12,13 +33,13 @@ function Login() {
           </div>
         </div>
 
-        <div class="right">
+        <div className="right">
           <h5>Login</h5>
 
-          <div class="inputs">
-            <input type="text" placeholder="user name" />
+          <div className="inputs">
+            <input type="text" ref={uniRef} placeholder="user name" />
             <br />
-            <input type="password" placeholder="password" />
+            <input type="password" ref={pdRef} placeholder="password" />
           </div>
 
           <br />
@@ -29,10 +50,18 @@ function Login() {
           </div>
 
           <br />
-          <button>Login</button>
+          <button onClick={handleLogin}>Login</button>
         </div>
       </div>
     </div>
+    
+
+
+
+
+
+
+
   );
 }
 
