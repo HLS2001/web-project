3ed9ab2f956cd56734395a451cd50ae0cf21ce1a
huynhleson <80943786+huynhleson@users.noreply.github.com>
2023-05-22 16:47:48 +0700

    Add files via upload

--- FE/src/Home.jsx
+++ FE/src/Home.jsx
@@ -1,19 +1,14 @@
 import { Link } from "react-router-dom";
 import Footer from "./Footer";
 import "./index.css";
-import { AiTwotoneStar, AiOutlineShoppingCart } from "react-icons/ai";
-import Product from "./Product";
 
 
-import { useGetALLProductsQuery } from "./features/productsApi";
-
-const Home=()=>{
-  
-  const {data,error, isLoading} = useGetALLProductsQuery();
-  
-}
+import axios from "axios";
+import { useCallback, useEffect, useState } from "react";
+import useFetchProduct from "./hooks/useFetchProduct";
 
 function Home() {
+
   return (
     <div className="App">
       <div className="mid">
@@ -21,65 +16,12 @@ function Home() {
         <h2>Super value deals</h2>
         <h1>On all products</h1>
         <p>Save more with coupons & up to 70% off!</p>
-        <Link to="shop"><button>ShopNow</button></Link>
-        
+        <Link to="shop">
+          <button>ShopNow</button>
+        </Link>
       </div>
 
-      {/* <section id="product1" className="section-p1">
-        <h2>Featured Products</h2>
-        <p>Summer Collection New Morden design</p>
-        <div className="product-container">
-          <Product
-            brand="Adidas"
-            name={"Cartoon Astronaut T-Shirts"}
-            price={78}
-            url={"public/product/f1.jpg"}
-          />
-
-          <Product
-            brand="Adidas"
-            name={"Cartoon Astronaut T-Shirts"}
-            price={78}
-            url={"public/product/f2.jpg"}
-          />
-          <Product
-            brand="Adidas"
-            name={"Cartoon Astronaut T-Shirts"}
-            price={78}
-            url={"public/product/f3.jpg"}
-          />
-          <Product
-            brand="Adidas"
-            name={"Cartoon Astronaut T-Shirts"}
-            price={78}
-            url={"public/product/f4.jpg"}
-          />
-          <Product
-            brand="Adidas"
-            name={"Cartoon Astronaut T-Shirts"}
-            price={78}
-            url={"public/product/f5.jpg"}
-          />
-          <Product
-            brand="Adidas"
-            name={"Cartoon Astronaut T-Shirts"}
-            price={78}
-            url={"public/product/f6.jpg"}
-          />
-          <Product
-            brand="Adidas"
-            name={"Cartoon Astronaut T-Shirts"}
-            price={78}
-            url={"public/product/f7.jpg"}
-          />
-          <Product
-            brand="Adidas"
-            name={"Cartoon Astronaut T-Shirts"}
-            price={78}
-            url={"public/product/f8.jpg"}
-          />
-        </div>
-      </section> */}
+      
       <Footer />
     </div>
   );
--- FE/src/main.jsx
+++ FE/src/main.jsx
@@ -2,30 +2,12 @@ import React from "react";
 import ReactDOM from "react-dom/client";
 import App from "./App";
 import "./index.css";
-
-import { configureStore, getDefaultMiddleware } from "@reduxjs/toolkit";
-import { Provider } from "react-redux";
-import productsReducer, { productsFetch } from "./features/productsSlice";
-import { productsApi } from "./features/productsApi";
-
-
-const store = configureStore({
-  reducer: {
-    prodcuts: productsReducer,
-    [productsApi.reducerPath]:productsApi.reducer,
-  },
-  middleware: (getDefaultMiddleware)=>{
-    getDefaultMiddleware().concat(productsApi.middleware);
-  },
-});
-
-
-store.dispatch(productsFetch());
+import { CartProvider } from "./context/cart_context";
 
 ReactDOM.createRoot(document.getElementById("root")).render(
   <React.StrictMode>
-    <Provider store={store}>
+    <CartProvider>
       <App />
-    </Provider>
+    </CartProvider>
   </React.StrictMode>
 );
